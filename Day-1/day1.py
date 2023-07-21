0e50e53a31871333a3989a8135146431ab76ea5e
cyberholics <victorkingoshimua@gmail.com>
2023-07-21 10:46:42 +0100

    day 2

--- Day-1/day1.py
+++ Day-1/day1.py
@@ -1,5 +1,10 @@
-## Problem statement:
+##Problem statement:
 
+"""Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6 """
 
 ## Solution
 
